6a25789447be705180b2b9a3b8580c785b3c97db
Rovatiana Ramaroson <rovatiana.ramaroson1@gmail.com>
2025-10-05 12:57:55 +0300

    save

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { BrowserRouter } from "react-router-dom";
-// import { ThemeProvider } from "next-themes"; // Fournit la gestion des thèmes à l'application
-
-// import Routes from "./routes/Routes";
-// import env from "./config/env";
-
-// // Typage explicite du composant
-// const App: React.FC = () => {
-//   // Affiche un message de bienvenue dans la console pour l'environnement de développement
-//   console.log("WELCOME GUYS 🙂");
-
-//   return (
-//     // Fournit des thèmes à l'application via le composant ThemeProvider
-//     <ThemeProvider attribute="class">
-//       <div className="w-full">
-//         <BrowserRouter basename={`/${env.PREFIXE_URL}`}>
-//           <Routes />
-//         </BrowserRouter>
-//       </div>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
-
-
-
 import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
